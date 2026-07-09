45728487bfa38cc0c4d342fce5ccf1912e6a1d5f
streamlit-snowflake <sempakinc@gmail.com>
2026-07-09 23:16:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
         # st.write(ingredients_string)
         my_insert_stmt = """
         INSERT INTO smoothies.public.orders (ingredients, name_on_order)
-        VALUES ('""" + ingredients_string.strip() + """','""" + name_on_order + """')
+        VALUES ('""" + ingredients_string + """','""" + name_on_order + """')
     """
 
     # st.write(my_insert_stmt)
