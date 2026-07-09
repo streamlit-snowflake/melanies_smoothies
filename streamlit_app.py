46cd71a80d17a3cf35d5123fc2cb83cf74f1045f
streamlit-snowflake <sempakinc@gmail.com>
2026-07-09 18:28:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,19 +30,12 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + " "
-
-    smoothiefroot_response = requests.get(
-        "https://my.smoothiefroot.com/api/fruit/watermelon"
-    )
-
-    sf_df = st.dataframe(
-        smoothiefroot_response.json(),
-        use_container_width=True
-    )
-
-    # st.write(ingredients_string)
-
-    my_insert_stmt = """
+        st.subheader(fruit_chosen + ' Nuttrition Information')
+        smoothiefroot_response = requests.get(
+        "https://my.smoothiefroot.com/api/fruit/watermelon"+ fruit_chosen)
+        sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True)
+        # st.write(ingredients_string)
+        my_insert_stmt = """
         INSERT INTO smoothies.public.orders (ingredients, name_on_order)
         VALUES ('""" + ingredients_string.strip() + """','""" + name_on_order + """')
     """
